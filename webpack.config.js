4bb4062d51c36211405ce0b1b2147d298795cb72
jungjuyoung <nwwn1004@naver.com>
2021-11-27 23:37:35 +0900

    feat: added function what the html file to automatically bring in the script

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -7,4 +8,11 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Boilerplate',
+      template: path.resolve(__dirname, './src/template.html'), // template file
+      filename: 'index.html', // output file
+    }),
+  ],
 }

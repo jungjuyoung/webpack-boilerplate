a92bcbe6ddf58aa8f8e2058ee0848fcbdf85c741
jungjuyoung <nwwn1004@naver.com>
2021-11-28 03:56:22 +0900

    feat: set up devserver and loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,39 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
   },
   output: {
     path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    compress: true,
+    host: 'localhost',
+    port: 8000,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Webpack Boilerplate',
+      title: 'Webpack FrontEnd Boilerplate',
       template: path.resolve(__dirname, './src/template.html'), // template file
       filename: 'index.html', // output file
     }),
+    new CleanWebpackPlugin(),
   ],
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
 }

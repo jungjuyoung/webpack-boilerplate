ab0120c04f9c24201015250c5ac9a41a1d7bad35
jungjuyoung <nwwn1004@naver.com>
2021-11-29 02:08:46 +0900

    feat: set up srouce map

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+let mode = 'development'
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production'
+}
 
 module.exports = {
-  mode: 'development',
+  mode,
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
   },

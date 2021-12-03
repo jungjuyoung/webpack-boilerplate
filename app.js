af2b3b11816349428a2e0e27a1abd989476a16f9
jungjuyoung <nwwn1004@naver.com>
2021-12-03 22:04:59 +0900

    feat: eslint and prettier set up

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const ALERT = (msg) => window.alert(msg)
+// const ALERT = (msg) => window.alert(msg)
+
+foo(
+  reallyLogArg(),
+  omgToManyParameters(),
+  IShouldRefactorThi(),
+  isThereSeriouslyAnotherOne()
+);
+
+var foo = '';
+console.log();
